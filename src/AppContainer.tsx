e3ba151a520239d5e9918d6050f98d504ef1b34e
Kyle Arean-Raines <kyle@toasttab.com>
2022-02-17 16:40:52 -0500

    More refactoring + cleanup

--- src/AppContainer.tsx
+++ src/AppContainer.tsx
@@ -4,8 +4,7 @@ import moment from "moment"
 import { useEffect, useState } from "react"
 import AudioClipPlayer from "./AudioClipPlayer"
 import translationsJson from "./translations.json"
-import similarityJson from "./lexical-similarity-data-so.json"
-import { Guess, Language } from "./types"
+import { Guess, Language, SimilarityIndex, LanguageWithTranslation } from "./types"
 import { equalIgnoreCase, startsWithIgnoreCase } from "./util/stringUtil"
 import { LanguageList } from "./LanguageList"
 
@@ -17,17 +16,26 @@ export const AppContainer = () => {
     const [audioPlayerShowing, setAudioPlayerShowing] = useState(true)
     const [languages, setLanguages] = useState<Language[]>([])
     const [sortedLanguages, setSortedLanguages] = useState<Language[]>([])
+    const [similarityData, setSimilarityData] = useState<SimilarityIndex[]>([])
+    const [answerWithTranslation, setAnswerWithTranslation] = useState<LanguageWithTranslation>()
+
     const [showNativeSpeakers, setShowNativeSpeakers] = useState(false)
     const [showLanguageOrigin, setShowLanguageOrigin] = useState(false)
     const [showFlag, setShowFlag] = useState(false)
 
+    const loadSimilarityData = async (code: string) => {
+        return await require(`./lexical-similarity-data-${code}.json`)
+    }
+
     useEffect(() => {
         // parse languages
-        let languages = translationsJson.map(str => {
+        const languages = translationsJson.map(str => {
             return {
                 code: str.language.language,
                 name: str.language.name,
                 country: str.language.country,
+                nativeSpeakers: str.language.nativeSpeakers,
+                origin: str.language.origin,
             }
         })
         let sorted = [...languages] 
@@ -35,6 +43,20 @@ export const AppContainer = () => {
 
         setLanguages(languages)
         setSortedLanguages(sorted)
+
+        const today = moment().dayOfYear()
+        const offset = today % languages.length
+
+        const answer = languages[offset]
+
+        setAnswerWithTranslation({
+            language: answer,
+            translation: translationsJson[offset].translation,
+        })
+
+        loadSimilarityData(answer.code).then(data => 
+            setSimilarityData(data)
+        )
     }, [])
 
     useEffect(() => {
@@ -44,21 +66,13 @@ export const AppContainer = () => {
         }
     }, [solved])
 
-    if (!languages.length) {
+    if (!languages.length || !answerWithTranslation) {
         return <>Loading...</>
     }
 
-    const today = moment().dayOfYear()
-    const offset = today % languages.length
-
-    const answerWithTranslation = translationsJson[offset]
-    const answer = {
-        code: answerWithTranslation.language.language,
-        name: answerWithTranslation.language.name,
-        country: answerWithTranslation.language.country,
-    }
-    const answerName = answerWithTranslation['language']['name']
-    const sentence = answerWithTranslation['translation']
+    const answer = answerWithTranslation.language
+    const answerName = answerWithTranslation.language.name
+    const sentence = answerWithTranslation.translation
 
     const filteredLanguages = sortedLanguages.filter(l => {
         let notYetGuessed = guesses.find(g => equalIgnoreCase(l.name, g.language.name)) === undefined
@@ -77,11 +91,7 @@ export const AppContainer = () => {
             setCurrentGuess('')
         } else if (guessInLower.toLowerCase() === answerName.toLowerCase()) {
             setGuesses(guesses.concat({
-                language: {
-                    code: answerWithTranslation.language.language,
-                    name: answerWithTranslation.language.name,
-                    country: answerWithTranslation.language.country,
-                },
+                language: answerWithTranslation.language,
                 rank: 100,
             }))
             setSolved(true)
@@ -91,7 +101,7 @@ export const AppContainer = () => {
                 // incorrect guess
                 let newGuess = {
                     language: guessedLanguage,
-                    rank: similarityJson.find(row => row.l1 === answerName && equalIgnoreCase(row.l2, guessInLower))?.value,
+                    rank: similarityData.find(row => row.l1 === answerName && equalIgnoreCase(row.l2, guessInLower))?.value,
                 }
                 setError(`Guess again!`)
                 setGuesses(guesses.concat(newGuess))
@@ -160,12 +170,23 @@ export const AppContainer = () => {
                         :
                         <strong>Audio unavailable</strong>
                     }
-                    <h4>Number of native speakers:</h4>
-                    {showNativeSpeakers ? <h3>16M</h3> : <Button onClick={() => setShowNativeSpeakers(true)}>Show hint #1</Button>}
-                    <h4>Language origin:</h4>
-                    {showLanguageOrigin ? <h3>Afro-Asiatic</h3> : <Button onClick={() => setShowLanguageOrigin(true)}>Show hint #2</Button>}
+                    {answer.nativeSpeakers && 
+                        <>
+                            <h4>Number of native speakers:</h4>
+                            {showNativeSpeakers ? <h3>{answer.nativeSpeakers}</h3> : <Button onClick={() => setShowNativeSpeakers(true)}>Show hint</Button>}
+                        </>
+                    }
+                    {answer.origin && 
+                        <>
+                            <h4>Language origin:</h4>
+                            {showLanguageOrigin ? <h3>{answer.origin}</h3> : <Button onClick={() => setShowLanguageOrigin(true)}>Show hint</Button>}
+                        </>
+                    }
                     <h4>Flag:</h4>
-                    {showFlag ? <img style={{ marginLeft: 10 }} height={50} src="https://word-puzzles.s3.us-east-1.amazonaws.com/flags/somalia.png" /> : <Button onClick={() => setShowFlag(true)}>Show hint #3</Button>}
+                    {showFlag ? 
+                        <img style={{ marginLeft: 10 }} height={50} src={`https://word-puzzles.s3.us-east-1.amazonaws.com/flags/${answer.country.toLowerCase()}.png`} /> : 
+                        <Button onClick={() => setShowFlag(true)}>Show hint</Button>
+                    }
                     <br /><br />
                     {solved ? <h2>{answerName}</h2> : 
                         <FormGroup>
--- src/types.ts
+++ src/types.ts
@@ -7,4 +7,17 @@ export type Language = {
     code: string
     name: string
     country: string
+    nativeSpeakers?: string
+    origin?: string
+}
+
+export type SimilarityIndex = {
+    l1: string
+    l2: string
+    value: number
+}
+
+export type LanguageWithTranslation = {
+    language: Language
+    translation: string
 }
